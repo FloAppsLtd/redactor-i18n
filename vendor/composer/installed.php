80724eaa46194f9a471fd65f80330bbe73e51d11
BelegUS <cendrzak.szymon@gmail.com>
2026-05-22 16:17:31 +0200

    Added Composer files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'floapps/redactor-i18n',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2b81617a3bb563fded2779685b43aa68a2076a92',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'floapps/redactor-i18n' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2b81617a3bb563fded2779685b43aa68a2076a92',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
